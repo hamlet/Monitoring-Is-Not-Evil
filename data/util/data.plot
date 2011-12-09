134d2ad2fa89e2ac0438713f4b8b85bfd3fd2a73
Hamlet <h@mlet.me>
2011-12-09 15:07:26 +0100

    long tests

--- data/util/data.plot
+++ data/util/data.plot
@@ -1,5 +1,5 @@
 set yr [0:1023]
-plot "data" using 1:2 with lines, "data" using 1:3 with lines, "data" using 1:4 with impulses
+plot "data" using 1:2 with lines
 set terminal png
 set output "data.png"
 replot
--- data/util/data_avg.rb
+++ data/util/data_avg.rb
@@ -1,26 +1,40 @@
 #!/usr/bin/env ruby
-value = 0
-diff = 0
-number = 0
-diffmin = 1024
-diffmax = 0
+require "iconv"
+#  Add methods to Enumerable, which makes them available to Array
+module Enumerable
+  #  sum of an array of numbers
+  def sum
+    return self.inject(0){|acc,i|acc +i}
+  end
+  #  average of an array of numbers
+  def average
+    return self.sum/self.length.to_f
+  end
+  #  variance of an array of numbers
+  def sample_variance
+    avg=self.average
+    sum=self.inject(0){|acc,i|acc +(i-avg)**2}
+    return(1/self.length.to_f*sum)
+  end
+  #  standard deviation of an array of numbers
+  def standard_deviation
+    return Math.sqrt(self.sample_variance)
+  end
+end  #  module Enumerable
+
+values = []
+errors_timing = 0
+errors_overflow = 0
+time = 0
 while a = gets
-  /(\d+) (\d+) (\d+) (\d+)/.match(a) {|m|
+  a= Iconv.conv('utf-8//IGNORE','utf-8', a)
+  /(\d+) (\d+)/.match(a) {|m|
+    errors_timing += 1 if (time + 2 != m[1].to_i) 
+    errors_overflow += 1 if (m[2].to_i == 0 || m[2].to_i == 1023) 
     time = m[1].to_i
-    value += m[2].to_i
-d = m[4].to_i
-    diff += d
-    number += 1
-    diffmin = d if d < diffmin
-    diffmax = d if d > diffmax
+    values << m[2].to_i
   }
 end
-#if (number == 100) 
-puts (value / number)
-puts (diff / number)
-puts diffmin, diffmax
-#value = 0
-#number = 0
-#end
+puts "Number of samples", values.length, "Average", values.average, "Deviation", values.standard_deviation, "Variance", values.sample_variance, "Errors timing", errors_timing, "Errors overflow", errors_overflow
 
 
