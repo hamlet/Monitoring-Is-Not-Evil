3825ec6ae2769fc4ed882757b21a89fd8e18e674
Hamlet <h@mlet.me>
2011-12-14 19:07:08 +0100

    adapt script for average and deviation

--- data/util/data.plot
+++ data/util/data.plot
@@ -1,5 +1,5 @@
 set yr [0:1023]
-plot "data" using 1:2 with lines
+plot "data" using 1:2 title "BF" with lines, "data.avg" using 1:2 title "avg" with lines, "data.avg" using 1:3 title "deviation" with lines
 set terminal png
 set output "data.png"
 replot
--- data/util/data.sh
+++ data/util/data.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 sh prep.sh
 
-cat /dev/ttyUSB0 | sed -n -e '/%\(.*\)% |\(.*\)|/P' | sed -e 's/%\(.*\)% |\(.*\)|/\1 \2/' > data &
+cat /dev/ttyUSB0 | tee data.all | sed -n -e '/%\(.*\)% |\(.*\)|/P' | sed -e 's/%\(.*\)% |\(.*\)|/\1 \2/' > data &
 pid=$!
 sleep $1
 kill $pid
+
+grep \+\+ data.all | sed -e 's/++\(.*\)+ ~\(.*\)~ |\(.*\)|/\1 \2 \3/' > data.avg
 
 gnuplot -p data.plot 
 cat data | ./data_avg.rb
 
 if [ -n $2 ] ; then
   cp data $2.txt
+  mv data.avg $2.avg
   mv data.png $2.png
 fi
